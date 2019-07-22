0b12bc81a93888c236bfc480f1354a259ea186d6
Lev <lev-savranskiy@users.noreply.github.com>
2019-07-22 15:25:58 -0400

    Create guthub-colorer.js

---
+++ guthub-colorer.js
@@ -0,0 +1,1 @@
+javascript:ghels=document.getElementsByClassName("tooltipped");for(let e in ghels){let t=ghels[e],n=t.innerText&&t.innerText.trim(),r={"Review required":"orange",Approved:"green","Changes requested":"red"};r[n]&&t.setAttribute("style",`background-color:${r[n]};color:#fff!important;padding:1px 5px;`)}
